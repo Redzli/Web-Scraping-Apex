bbfd7080caae170217b02c5f839e4ba1060f6a17
Red Li <li.zhengyuan@mail.mcgill.ca>
2020-01-13 23:51:14 -0500

    scrape script created

--- config.py
+++
@@ -1,8 +0,0 @@
-github_account = 'li.zhengyuan@mail.mcgill.ca'
-github_password = 'iversonai971103'
-
-stack_name = 'Testing Entry'
-
-#stack_url must contain "https://" prefix 
-stack_url = 'https://www.google.com'
-
